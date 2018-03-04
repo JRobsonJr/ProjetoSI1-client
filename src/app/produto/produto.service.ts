cd06fb934d8d50c872e396539fb250b3a3f3b327
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2018-03-04 20:46:13 -0300

    Mudando requisição

--- src/app/produto/produto.service.ts
+++ src/app/produto/produto.service.ts
@@ -18,7 +18,7 @@ export class ProdutoService {
 
   getProdutos() {
     return this.http
-      .get('http://localhost:8080/produto')
+      .get('https://estoque-facil-client.herokuapp.com/produto')
       .map(result => (this.result = result.json() as Array<Produto>));
   }
 

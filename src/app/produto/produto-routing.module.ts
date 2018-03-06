a07abc2977f8871cf6af398f83ed39ba18c50c78
Paulo Leitao <paulojbleitao@gmail.com>
2018-03-05 23:56:30 -0300

    criando component de atualiza-produto

--- src/app/produto/produto-routing.module.ts
+++ src/app/produto/produto-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CadastraProdutoComponent } from './cadastra-produto/cadastra-produto.component';
+import { AtualizaProdutoComponent } from './atualiza-produto/atualiza-produto.component';
 import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 
 const routes: Routes = [
   { path: 'produto/cadastrar', component: CadastraProdutoComponent },
+  { path: 'produto/:id', component: AtualizaProdutoComponent },
   { path: 'produtos', component: ListaProdutosComponent }
 ];
 

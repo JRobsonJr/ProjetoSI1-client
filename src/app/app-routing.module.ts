006591fbd5624e32e436998ff9e1c15ca26aee7e
Robson Junior <jose.robson.junior@ccc.ufcg.edu.br>
2018-03-08 15:16:35 -0300

    Redirecionando página inicial para tela de login/painel de controle.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/painel-de-controle', pathMatch: 'full', canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

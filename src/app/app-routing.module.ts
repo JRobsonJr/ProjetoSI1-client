6edacdbcfa60a8c0f6e8bcb8f0a432912b96eb47
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2018-03-10 14:34:39 -0300

    Removido pequeno problema de rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '', redirectTo: '/painel-de-controle', pathMatch: 'full', canActivate: [AuthGuard] }
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({

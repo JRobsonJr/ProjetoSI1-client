66088e1303bf4d69a8625066cb21849a440b7b1b
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2018-03-10 14:41:03 -0300

    Métodos de VendaService declarados.

--- src/app/venda/venda.service.ts
+++ src/app/venda/venda.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class VendaService {
+  private baseUrl = 'https://estoque-facil-server.herokuapp.com/venda/';
 
   constructor() { }
 
+  cadastraVenda() {
+
+  }
+
+  listaVendas() {
+
+  }
+
+  recuperaTotalVendas() {
+
+  }
+
+  cancelaVenda() {
+
+  }
 }

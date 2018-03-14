1534e5825de7154ae93c760e385ef82d09150c7b
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2018-03-14 03:51:20 -0300

    Tudo relacionado a notificações implementado.

--- src/app/notificacoes/notificacoes.component.ts
+++ src/app/notificacoes/notificacoes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { NotificacoesService } from './notificacoes.service';
+import { Produto } from '../produto/produto.model';
 
 @Component({
   selector: 'app-notificacoes',
@@ -7,10 +9,25 @@ import { NotificacoesService } from './notificacoes.service';
   styleUrls: ['./notificacoes.component.css']
 })
 export class NotificacoesComponent implements OnInit {
+  produtosVencimentoProximo: Array<Produto>;
+  produtosBaixaQtd: Array<Produto>;
 
-  constructor(private notificacoesService: NotificacoesService) { }
-
-  ngOnInit() {
+  constructor(private notificacoesService: NotificacoesService) {
+    this.produtosBaixaQtd = new Array<Produto>();
+    this.produtosVencimentoProximo = new Array<Produto>();
   }
 
+  ngOnInit() {
+    this.atualizaProdutos();
+  }
+
+  atualizaProdutos() {
+    this.notificacoesService
+      .listaProdutosBaixaQuant()
+      .subscribe(produtos => (this.produtosBaixaQtd = produtos));
+
+    this.notificacoesService
+      .listaProdutosVencimentoProximo()
+      .subscribe(produtos => (this.produtosVencimentoProximo = produtos));
+  }
 }
